f1bc80d743fdfdfed60ae3090dd212e87f41f21c
Ruslan Digoev <rdigoev@mapsoft.by>
2022-07-29 15:33:47 +0300

    Update settings

--- bonkbonkback/settings.py
+++ bonkbonkback/settings.py
@@ -147,7 +147,7 @@ else:
     AWS_S3_REGION_NAME = os.environ.get('BUCKETEER_AWS_REGION')
     AWS_DEFAULT_ACL = None
     # AWS_S3_SIGNATURE_VERSION = os.environ.get('S3_SIGNATURE_VERSION', 's3v4')
-    AWS_S3_ENDPOINT_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    AWS_S3_ENDPOINT_URL = f'https://s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
     STATIC_DEFAULT_ACL = 'public-read'
